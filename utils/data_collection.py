344cf3e183f03f2c61313b18d07b5fb5bc3825ac
mvigneron546 <mvigneron546@gmail.com>
2023-07-05 16:33:38 +0200

    using config file

--- utils/data_collection.py
+++ utils/data_collection.py
@@ -1,13 +1,18 @@
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
+import yaml
 
 # Don't forget to run ``az login`` in the command prompt and authenticate!
 
-account_url = "https://firefighterdata.blob.core.windows.net"
+with open("../config.yml", "r") as f:
+    config = yaml.safe_load(f)
+
+ACCOUNT_URL = config['data']['account_url']
+CONTAINER_NAME = config['data']['azure_container_name']
 default_credential = DefaultAzureCredential()
 
 # Create the BlobServiceClient object
-blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+blob_service_client = BlobServiceClient(ACCOUNT_URL, credential=default_credential)
 
 def upload_file_to_blob(blob_service_client: BlobServiceClient, container_name: str, filepath: str):
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=filepath)
@@ -23,5 +28,5 @@ def download_blob_to_file(blob_service_client: BlobServiceClient, container_name
         sample_blob.write(download_stream.readall())
 
 
-download_blob_to_file(blob_service_client, 'data', "LFB Mobilisation data from January 2009.zip")
-upload_file_to_blob(blob_service_client, 'data', 'requirements.txt')
+download_blob_to_file(blob_service_client, CONTAINER_NAME, "LFB Mobilisation data from January 2009.zip")
+upload_file_to_blob(blob_service_client, CONTAINER_NAME, 'requirements.txt')
